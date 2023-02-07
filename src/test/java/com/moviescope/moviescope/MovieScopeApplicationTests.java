b0bf3a7134c01299247cb83141d68dfb5c1dd90b
Yashpal <yashpal.singh@ongrid.in>
2023-02-07 17:08:37 +0530

    Inital commit

---
+++ src/test/java/com/moviescope/moviescope/MovieScopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviescope.moviescope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieScopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
